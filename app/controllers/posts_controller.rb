73776ec369baa46456cede14911ceb431c1538bc
Jessica Rowland <rowlandjl@vcu.edu>
2018-08-12 23:42:02 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,9 +4,9 @@ class PostsController < ApplicationController
   end
 
   def new
-  end 
+  end
 
-  def create 
+  def create
     Post.create(title: params[:post][:title], description: params[:post][:description])
     redirect_to posts_path
   end 

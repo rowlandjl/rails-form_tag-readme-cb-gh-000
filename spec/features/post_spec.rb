d57f3418029960d0afaa99ae02be5aa7d3a82f3a
Jessica Rowland <rowlandjl@vcu.edu>
2018-08-12 23:31:55 +0000

    automatically backed up by learn

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -9,5 +9,15 @@ describe 'new post' do
   it 'renders HTML in the /new template' do
     visit new_post_path
     expect(page).to have_content('Post Form')
-  end 
+  end
+
+  it "displays a new post form that redirects to the index page,
+    which then contains the submitted post's title and description" do
+    visit new_post_path
+    fill_in 'post_title', with: 'My post title'
+    fill_in 'post_description', with: 'My post description'
+
+    click_on 'Submit Post'
+
+    
 end
